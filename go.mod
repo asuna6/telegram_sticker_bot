4e061f067ee40607bc425965b27c96ac9fdf0149
asuna <jiabaili.top@gmail.com>
2023-11-26 01:28:13 +0000

    添加sticker bot的代码

---
+++ go.mod
@@ -0,0 +1,10 @@
+module telegram_sticker_bot
+
+go 1.21.4
+
+require (
+	github.com/codeskyblue/go-sh v0.0.0-20200712050446-30169cf553fe
+	gopkg.in/telebot.v3 v3.2.1
+)
+
+require github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
---
+++ main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/codeskyblue/go-sh"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	pref := tele.Settings{
+		Token:  "6341723545:AAHoxWlv_ME_c02zk84A3PcNBbZoO8RA_AA",
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+		// Poller: webhook,
+	}
+
+	bot, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	log.Printf("%s 启动", bot.Me.Username)
+
+	commands := []tele.Command{{
+		Text:        "get_id",
+		Description: "发送此命令获取用户和群组的id",
+	}}
+
+	_ = bot.SetCommands(commands)
+
+	bot.Handle("/get_id", func(c tele.Context) error {
+		if c.Chat().Type == tele.ChatGroup || c.Chat().Type == tele.ChatSuperGroup {
+			var msg = fmt.Sprintf("用户id:%d\n群组id:%d", c.Message().OriginalSender.ID, c.Message().Chat.ID)
+			return c.Send(msg, tele.ModeHTML)
+		} else {
+			var msg = fmt.Sprintf("用户id:%d\n对话id:%d", c.Message().OriginalSender.ID, c.Message().Chat.ID)
+			return c.Send(msg, tele.ModeHTML)
+		}
+	})
+
+	bot.Handle(tele.OnSticker, func(c tele.Context) error {
+		// 把sticker转换为合适的格式发送给用户
+		sticker := c.Message().Sticker
+		stickerSet := sticker.SetName
+		baseDir, _ := os.Getwd()
+		dir := fmt.Sprintf("%s/%s", baseDir, stickerSet)
+		// 创建文件夹
+		_, err := os.Stat(dir)
+		if err != nil {
+			if os.IsNotExist(err) {
+				err = os.Mkdir(dir, os.ModePerm)
+				if err != nil {
+					return c.Send(err.Error(), tele.ModeHTML)
+				}
+			} else {
+				return c.Send(err.Error(), tele.ModeHTML)
+			}
+		}
+
+		if sticker.Animated {
+			if sticker.Type != tele.StickerRegular {
+				return c.Send(fmt.Sprintf("不支持的sticker类型:%s", sticker.Type), tele.ModeHTML)
+			}
+
+			tgsFileName := fmt.Sprintf("%s/%s.tgs", dir, sticker.UniqueID)
+			gifFileName := strings.ReplaceAll(tgsFileName, ".tgs", ".tgs.gif")
+
+			_, err = os.Stat(tgsFileName)
+			if err != nil {
+				if os.IsNotExist(err) == false {
+					return c.Send(err.Error(), tele.ModeHTML)
+				}
+
+				err := bot.Download(sticker.MediaFile(), tgsFileName)
+				if err != nil {
+					return c.Send(err.Error(), tele.ModeHTML)
+				}
+
+				_, err = os.Stat(gifFileName)
+				if err != nil {
+					if os.IsNotExist(err) == false {
+						return c.Send(err.Error(), tele.ModeHTML)
+					}
+
+					if runtime.GOOS != "linux" {
+						return c.Send("请在linux中执行", tele.ModeHTML)
+					}
+					// 使用第三方的docker镜像转换tgs为gif, 参考 https://github.com/ed-asriyan/lottie-converter
+					cmd := sh.Command("docker", "run", "--rm", "-v", fmt.Sprintf("%s:/source", dir), "edasriyan/lottie-to-gif")
+					cmd.ShowCMD = true
+					output, err := cmd.CombinedOutput()
+					if err != nil {
+						fmt.Println(fmt.Sprintf("执行命令失败,错误信息:%s", err.Error()))
+						return c.Send(string(output), tele.ModeHTML)
+					}
+				}
+			}
+
+			imageFile := tele.Document{File: tele.FromDisk(gifFileName), FileName: fmt.Sprintf("%s.gif", sticker.UniqueID), Caption: "MP4格式的动图,下载后请自行转换格式为gif"}
+			err = c.Send(&imageFile)
+			if err != nil {
+				return c.Send(err.Error(), tele.ModeHTML)
+			} else {
+				return nil
+			}
+		} else if sticker.Video {
+			if sticker.Type != tele.StickerRegular {
+				return c.Send(fmt.Sprintf("不支持的sticker类型:%s", sticker.Type), tele.ModeHTML)
+			}
+
+			webmFileName := fmt.Sprintf("%s/%s.webm", dir, sticker.UniqueID)
+			mp4FileName := strings.ReplaceAll(webmFileName, ".webm", ".mp4")
+			_, err = os.Stat(webmFileName)
+			if err != nil {
+				if os.IsNotExist(err) == false {
+					return c.Send(err.Error(), tele.ModeHTML)
+				}
+
+				err := bot.Download(sticker.MediaFile(), webmFileName)
+				if err != nil {
+					return c.Send(err.Error(), tele.ModeHTML)
+				}
+
+				// windows下请手动下载ffmpeg并且放到环境变量目录中
+				// 从go 1.19以后不能把可执行文件放在当前目录
+				// 否则会产生一个报错类似于: exec: "ffmpeg": cannot run executable found relative to current directory
+				// linux下可以自动下载 ffmpeg
+				// 如果转换为gif格式会损失图像质量, 效果很不好
+				cmd := sh.Command("ffmpeg", "-i", webmFileName, mp4FileName)
+				cmd.ShowCMD = true
+				output, err := cmd.CombinedOutput()
+				if err != nil {
+					fmt.Println(fmt.Sprintf("执行命令失败,错误信息:%s", err.Error()))
+					return c.Send(string(output), tele.ModeHTML)
+				}
+			}
+
+			mp4File := tele.Video{File: tele.FromDisk(mp4FileName)}
+			err = c.Send(&mp4File)
+			if err != nil {
+				return c.Send(err.Error(), tele.ModeHTML)
+			} else {
+				return nil
+			}
+		} else {
+			if sticker.Type == tele.StickerRegular {
+				webpFileName := fmt.Sprintf("%s/%s.webp", dir, sticker.UniqueID)
+				err := bot.Download(sticker.MediaFile(), webpFileName)
+				if err != nil {
+					return c.Send(err.Error(), tele.ModeHTML)
+				}
+
+				// 原始图片是webp格式, 因为有很多webp图片是透明背景, 转换为jpg格式会有黑色背景, 转换为png也有奇怪的问题, 用户可以下载后自行转换和处理
+				imageFile := tele.Photo{File: tele.FromDisk(webpFileName)}
+				err = c.Send(&imageFile)
+				if err != nil {
+					return c.Send(err.Error(), tele.ModeHTML)
+				} else {
+					return nil
+				}
+			}
+			return c.Send("待完成", tele.ModeHTML)
+		}
+	})
+
+	bot.Start()
+}
